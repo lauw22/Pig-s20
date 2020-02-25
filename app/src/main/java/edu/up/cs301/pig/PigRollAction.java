ab9b6031f95e00861acc6e06b590770022ca5d16
William Lau <iamwill8888@yahoo.com>
2020-02-24 19:59:27 -0800

    commit 2

--- app/src/main/java/edu/up/cs301/pig/PigRollAction.java
+++ app/src/main/java/edu/up/cs301/pig/PigRollAction.java
@@ -3,15 +3,13 @@ package edu.up.cs301.pig;
 import edu.up.cs301.game.GameFramework.GamePlayer;
 import edu.up.cs301.game.GameFramework.actionMessage.GameAction;
 
-public class PigHoldAction extends GameAction {
-
-
+public class PigRollAction extends GameAction {
     /**
      * constructor for GameAction
      *
      * @param player the player who created the action
      */
-    public PigHoldAction(GamePlayer player) {
+    public PigRollAction(GamePlayer player) {
         super(player);
     }
 }
